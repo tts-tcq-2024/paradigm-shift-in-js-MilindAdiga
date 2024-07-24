4db1831846a12ac8d14a92dd0366d694a76648ba
MilindAdiga <124535083+MilindAdiga@users.noreply.github.com>
2024-07-24 19:11:26 +0530

    Update checker.js

--- checker.js
+++ checker.js
@@ -1,34 +1,39 @@
-function batteryIsOk(temperature,  soc,  chargeRate) {
-    if(temperature < 0 || temperature > 45) {
+const TEMPERATURE_WARNING_THRESHOLD = 0.05 * 45; // 5% of 45
+const SOC_WARNING_THRESHOLD = 0.05 * 80;         // 5% of 80
+const CHARGE_RATE_WARNING_THRESHOLD = 0.05 * 0.8; // 5% of 0.8
+
+function checkTemperature(temperature) {
+    if (temperature < 0 || temperature > 45) {
         console.log("Temperature is out of range!");
         return false;
-    } else if(soc < 20 || soc > 80) {
-        console.log("State of Charge is out of range!");
-        return false;
-    } else if(chargeRate > 0.8) {
-        console.log("Charge Rate is out of range!");
-        return false;
+    } else if (temperature <= 0 + TEMPERATURE_WARNING_THRESHOLD) {
+        console.log("Warning: Temperature approaching lower limit");
+    } else if (temperature >= 45 - TEMPERATURE_WARNING_THRESHOLD) {
+        console.log("Warning: Temperature approaching upper limit");
     }
     return true;
 }
 
-function  ExpectTrue(expression) {
-    if(!expression) {
-        console.log("Expected true, but got false");
-        
+function checkSoc(soc) {
+    if (soc < 20 || soc > 80) {
+        console.log("State of Charge is out of range!");
+        return false;
+    } else if (soc <= 20 + SOC_WARNING_THRESHOLD) {
+        console.log("Warning: Approaching discharge");
+    } else if (soc >= 80 - SOC_WARNING_THRESHOLD) {
+        console.log("Warning: Approaching charge-peak");
     }
-}
-function ExpectFalse(expression) {
-    if(expression) {
-        console.log("Expected false, but got true");
-        Environment.Exit(1);
-    }
-}
-function main() {
-    ExpectTrue(batteryIsOk(25, 70, 0.7));
-    ExpectFalse(batteryIsOk(50, 85, 0.0));
-    console.log("All ok");
-    return 0;
+    return true;
 }
 
-main();
+function checkChargeRate(chargeRate) {
+    if (chargeRate > 0.8) {
+        console.log("Charge Rate is out of range!");
+        return false;
+    } else if (chargeRate >= 0.8 - CHARGE_RATE_WARNING_THRESHOLD) {
+        console.log("Warning: Charge rate approaching upper limit");
+    }
+    return true;
+}
+
+module.exports = { checkTemperature, checkSoc, checkChargeRate };
